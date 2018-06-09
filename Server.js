178ebb2c40fb7653bef3bff7d4cf5a53b00e44e4
Sebastian-Yang <yangziliaa@126.com>
2018-06-09 21:33:45 +0800

    Update Server.js

--- Server.js
+++ Server.js
@@ -14,32 +14,35 @@ var server = app.listen(port,function(){
 var io = socket(server);
 var people_cnt = 0;
 
+//Client connect to server
 io.on('connection',function(socket){
 	people_cnt++;
 
+	//Client disconnect from the server
 	socket.on('disconnect', function(){
 		people_cnt--;
 		console.log(socket.id + ' is disconnected.')
-		socket.broadcast.emit('people_out', {
+		socket.broadcast.emit('people_out', {		//Broadcast to all online users
 			id: socket.name,
 			cnt: people_cnt
 		})
 	});
 
+	//Recieve messaga
 	socket.on('message', function(data){
 		// console.log(socket.id + '  ' + msg);
-		socket.broadcast.emit('message', {
-			message: data.message,
+		socket.broadcast.emit('message', {			//Broadcast to all online users
+			message: data.message,					//except the one who fire the event
 			source_id: data.id,
-			source_token: data.token
 		});
 	});
 
+	//Recieve user's ID
 	socket.on('data',function(data){
-		socket.name = data.id
+		socket.name = data.id;						//Pass user's ID to object socket's name parameter
 		console.log(socket.name + ' is connected.')
 		socket.broadcast.emit('people_in', {
-			socket_id: socket.id,
+			socket_id: socket.name,
 			cnt: people_cnt
 		})
 		console.log('There is ' + people_cnt + ' people online.')
@@ -48,15 +51,17 @@ io.on('connection',function(socket){
 
 });
 
-
+//Collect admin's input data
 cin.on('data', function(chunk){
 	if(chunk.toString() != '\r\n'){
 		msg = (chunk+'').replace(/[\r\n]/ig,"");
 
+		//If the string is 'exit' or 'quit' then close the server
 		if(msg.toLowerCase() == 'exit' || msg.toLowerCase() == 'quit'){
 			console.log("Server closed.")
 			io.close();
 		}else{
+			//Otherwise broadcast to all users as administrator
 			io.sockets.emit('message',{
 				message: msg,
 				source_id: "admin",
@@ -65,3 +70,4 @@ cin.on('data', function(chunk){
 
 	}
 });
+
